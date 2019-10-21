3456f8f3dfe6af005fc8e480e991a2b9895e1a27
DeuxMax <maxime.maillet93@gmail.com>
2019-10-21 17:42:06 +0200

    add talk in footer

--- src/Routes/Post.js
+++ src/Routes/Post.js
@@ -8,6 +8,19 @@ import Footer from "../Components/Footer/Footer";
 export class Post extends Component {
   componentDidMount() {
     document.title = this.props.title ? this.props.title : 'Article';
+    (function() {
+        var talk = document.createElement('script'); talk.type = 'text/javascript'; talk.async = true;
+        var url = process.env.REACT_APP_TALK_URL;
+        talk.src = '//' + url + '/assets/js/embed.js';
+        talk.onload = function() {
+            window.Coral.createStreamEmbed({
+                id: "coral_thread",
+                autoRender: true,
+                rootURL: '//' + url,
+            });
+        };
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(talk);
+    })();
   }
 
   componentWillReceiveProps(nextProps, nextContext) {
@@ -25,6 +38,7 @@ export class Post extends Component {
         <div className="container">
           <OnePost {...this.props} className="resp-overlap-top-40" />
         </div>
+        <div id="coral_thread"></div>
         <Footer
           image={this.props.feature_image}
         />
